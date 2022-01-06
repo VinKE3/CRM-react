77423c8dd1b375e0813c908c29169515195dfe6e
VinKE3 <harquin09@gmail.com>
2022-01-06 14:32:38 -0300

    commit2

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
   const location = useLocation();
-  console.log(location);
+  const urlActual = location.pathname;
   return (
     <div className="md:flex md:min-h-screen">
       <div className="md:w-1/4 bg-blue-600 px-5 py-10">
@@ -11,13 +11,17 @@ const Layout = () => {
         </h2>
         <nav className="mt-10">
           <Link
-            className="text-white text-2xl block mt-2 hover:text-blue-200"
+            className={`${
+              urlActual === "/clientes" ? "text-blue-300" : "text-white"
+            }  text-2xl block mt-2 hover:text-blue-200`}
             to="/clientes"
           >
             Clientes
           </Link>
           <Link
-            className="text-white text-2xl block mt-2 hover:text-blue-200"
+            className={`${
+              urlActual === "/clientes/nuevo" ? "text-blue-300" : "text-white"
+            }  text-2xl block mt-2 hover:text-blue-200`}
             to="/clientes/nuevo"
           >
             Nuevo Cliente
